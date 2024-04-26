b9c067389e9988d174c93b09aa04ec30dd34e286
Abraham <morelosguerrero1109abraham@gmail.com>
2024-04-26 16:00:10 -0600

    Ruta myname

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3200;
+
 app.get("/", (req, res) => {
   res.send(`Hello World from Port ${port}`);
 });
 
-app.listen(3000, () => {
+app.get("/myname", (req, res) => {
+  const name = "!Abraham Raul!";
+  res.send(`Hello World, My name is ${name}.`);
+});
+
+app.listen(port, () => {
   console.info(`Server is running on port ${port}`);
 });
